aacd59e3c90a3f8d421d428eed284e457d146864
unknown <15608349071@163.com>
2019-03-21 15:41:10 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,32 @@
+# 声明一个int_demo的方法
+def int_demo():
+    # 声明aint变量 并赋值 1
+    aint = 1
+    # 打印 aint的值
+    print(aint)
+
+
+# 声明一个min的方法 参数有两个
+def min(aint, bint):
+    # 打印 aint 的值
+    print(aint)
+    # 打印 bint 的值
+    print(bint)
+    #
+    return aint - bint
+
+def afloat_demo():
+    # 声明afloat变量 ， 并赋值 1.8
+    afloat = 1.8
+    # 打印 afloat 的值
+    print(afloat)
+    # 打印 afloat 的类型， type(afloat): 获取afloat的类型
+    print(type(afloat))
+
+
+if __name__ == '__main__':
+    afloat_demo()
+    # int_demo()
+    # result = min(4, 2)
+    # print(result)
+    pass
